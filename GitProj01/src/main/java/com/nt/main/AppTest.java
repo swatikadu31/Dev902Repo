5472269381e0b4b312ea35f439e30f0fe4e060ab
Lenovo <Lenovo@DESKTOP-45MESNG.bbrouter>
2024-02-29 18:06:12 +0530

    task1 from Dev1

--- GitProj01/src/main/java/com/nt/main/AppTest.java
+++ GitProj01/src/main/java/com/nt/main/AppTest.java
@@ -1,10 +1,14 @@
 package com.nt.main;
 
+import com.nt.services.ArithmaticOperations;
+
 public class AppTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+ArithmaticOperations ar=new ArithmaticOperations();
+System.out.println("sum:"+ar.sum(100,200));
+System.out.println("sub:"+ar.sub(100,200));
 	}
 
 }
---
+++ GitProj01/src/main/java/com/nt/services/ArithmaticOperations.java
@@ -0,0 +1,13 @@
+package com.nt.services;
+
+public class ArithmaticOperations {
+
+	public int sum(int x,int y)
+	{
+		return x+y;
+	}
+	public int sub(int x,int y)
+	{
+		return x-y;
+	}
+}
