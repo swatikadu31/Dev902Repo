de67bb349be9667edaf4da60a20e2235f2cd3bfc
Lenovo <Lenovo@DESKTOP-45MESNG.bbrouter>
2024-03-05 16:54:13 +0530

    conflict-code1-TL

--- GitProj01/src/main/java/com/nt/main/AppTest.java
+++ GitProj01/src/main/java/com/nt/main/AppTest.java
@@ -10,7 +10,7 @@ ArithmaticOperations ar=new ArithmaticOperations();
 System.out.println("sum:"+ar.sum(100,200));
 System.out.println("sub:"+ar.sub(100,200));
 System.out.println("----------------------------");
-System.out.println("mul:"+ar.mul(100,300));
+System.out.println("mul:"+ar.mul(100,300));//456
 	}
 
 }
