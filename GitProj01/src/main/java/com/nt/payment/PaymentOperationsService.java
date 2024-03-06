3c7fc95a6998560d03147c2932758ac9be38be2b
Lenovo <Lenovo@DESKTOP-45MESNG.bbrouter>
2024-03-06 11:00:46 +0530

    Dev1-b1-task1

--- GitProj01/src/main/java/com/nt/payment/PaymentOperationsService.java
+++ GitProj01/src/main/java/com/nt/payment/PaymentOperationsService.java
@@ -1,5 +1,12 @@
 package com.nt.payment;
 
 public class PaymentOperationsService {
-
+public String doCreditCardPayment()
+{
+	return "Credit card Payment is done";
+}
+public String doDebitCardPayment()
+{
+	return "Debit card Payment is done";
+}
 }
